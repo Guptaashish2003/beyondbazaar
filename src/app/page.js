29dc2947a0c5b071aeeac35fb6cf975764ca1958
Rajneesh <rajneeshkumar9674737@gmail.com>
2023-09-22 16:00:53 +0530

    product card completed

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import Category from '@/components/Category/Category'
 import Herosection from '@/components/HeroSection/HeroSection'
-import Footer from '@/components/footer/Footer'
+import Productcard from '@/components/ProductCards/Productcard'
 
 
 export default function Home() {
@@ -8,7 +8,17 @@ export default function Home() {
     <>
     <Herosection/>
     <Category/>
-    <Footer/>
+    <h1 className='p-8 text-5xl font-bold uppercase text-center'>Select Your Category</h1>
+    <div className='flex flex-wrap justify-center'>
+    <Productcard/>
+    <Productcard border={"black"}/>
+    <Productcard animation={true}/>
+    <Productcard animation={true}/>
+    <Productcard  discount={600} />
+    <Productcard discount={600} bgColor={"#e2e1e6"}/>
+    <Productcard addToCart={true}/>
+    <Productcard addToCart={true}/>
+    </div>
     </>
   )
 }
