56b8b6dd5ab51614f611d581f1f76a53140322f5
Rajneesh <rajneeshkumar9674737@gmail.com>
2023-09-23 12:53:50 +0530

    added login end point

---
+++ src/app/(pages)/login/page.js
@@ -0,0 +1,10 @@
+import Login from '@/components/login/Login'
+import React from 'react'
+
+function page() {
+  return (
+    <Login/>
+  )
+}
+
+export default page
--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,14 +1,25 @@
-
-import React from 'react'
+"use client"
+import {React,useRef} from 'react'
 import Image from 'next/image'
 import category1 from "@/assets/category1.png"
 function Category() {
     const category = [1,2,3,4,5,6,7,8,9,10,11,12]
+    const ref = useRef();
+
+    const onWheel = (e) => {
+      const elelemnt = ref.current;
+      if (elelemnt) {
+        if (e.deltaY == 0) return;
+        elelemnt.scrollTo({
+          left: elelemnt.scrollLeft + e.deltaY,
+        });
+      }
+    };
 
   return (
     <div className='flex flex-col justify-center items-center text-center overflow-hidden '>
         <h1 className='p-8 text-5xl font-bold uppercase'>Select Your Category</h1>
-        <div className='w-full flex overflow-x-scroll cursor-grab' >
+        <div className='w-full flex overflow-x-scroll cursor-grab' ref={ref} onWheel={onWheel}>
            { category.map((cate,index)=><div key={index}>
                 <div className="category-card m-3 h-72 w-60 flex flex-col items-center justify-evenly" >
                     <Image
