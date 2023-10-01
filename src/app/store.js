771c2f7e660389cbf22a02e798b1134515f3a32a
Guptaashish2003 <ashish.10119011622@ipu.ac.in>
2023-10-01 10:56:43 +0530

    path change

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,6 @@
 import { legacy_createStore as createStore} from 'redux'
 
-import rootReducer from "./action/reducers/index";
+import rootReducer from "./reducers/index";
 
 const store = createStore(rootReducer);
 export default store;
