06e94f9624a2b81ee68bcc8655864ee863a1b75b
Rajneesh <rajneeshkumar9674737@gmail.com>
2023-10-28 12:30:50 +0530

    fix feature card

--- src/components/singleProduct/ProductDetails.jsx
+++ src/components/singleProduct/ProductDetails.jsx
@@ -34,16 +34,20 @@ const ProductDetails = () => {
             Specification
           </a>
         </div>
-          <div className="p-6 mt-4 lg:ml-4 text-gray-600 dark:text-gray-500">
+          <div className="p-6 mt-4 lg:ml-4 text-[--first-color] dark:text-gray-500">
             {showDescription ? (
               <div className="lg:ml-4">
-                <h2 className="text-2xl font-semibold ">Description</h2>
+                <h2 className="text-2xl font-semibold text-[--first-color]">Description</h2>
                 <p>Insert your description text here.</p>
+                <div className="mt-2 text-base">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis nulla dolor distinctio soluta, debitis ullam! Placeat deserunt totam a animi soluta explicabo eius maiores quis voluptate iste accusamus, veniam iusto quos sit minima error? Consequuntur exercitationem saepe id porro sed, sapiente vero quia, natus laborum culpa nobis laudantium quidem nulla vel dolore similique veritatis dignissimos assumenda consectetur laboriosam maxime totam numquam. Suscipit eius possimus architecto accusantium ea asperiores animi unde error tempora.
+                </div>
               </div>
             ) : (
               <div className="lg:ml-4">
-                <h2 className="text-2xl  font-semibold">Specification</h2>
+                <h2 className="text-2xl  font-semibold text-[--first-color]">Specification</h2>
                 <p>Insert your specification text here.</p>
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, magni? Rerum, assumenda. Necessitatibus eum eligendi, fugit ex libero, maiores recusandae tempore repellat eius placeat autem voluptatibus nisi dolore saepe odit exercitationem nesciunt sed. At illum voluptatem vitae placeat, animi totam. Enim in debitis libero ex ipsam molestias dolore sint id eos molestiae impedit, aspernatur quia rem ratione dolorem optio quidem tempora nihil pariatur. Eum impedit officiis similique vel officia nesciunt facere quisquam.</div>
               </div>
             )}
           </div>
