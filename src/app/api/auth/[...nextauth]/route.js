34219710a6da8d4c05a82d6d0f0c247dddd786a6
Rajneesh <rajneeshkumar9674737@gmail.com>
2023-11-05 13:16:48 +0530

    admin pakage added

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -2,12 +2,20 @@ import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
 const handler = NextAuth({
+    session: {
+      strategy: "jwt",
+    },
   providers:[
     GoogleProvider({
         clientId:process.env.ClIENT_ID,
         clientSecret:process.env.CLIENT_SECRET,
     })
   ],
+  
+    pages: {
+      signIn: "/login",
+    },
+    secret: process.env.GOOGLE_AUTH_API_KEY,
 //   pages: {
 //     signIn: '/auth/dashbaord', // on successfully signin    
 //     signOut: '/auth/login', // on signout redirects users to a custom login page.
