4c1a5c936267a8179694f0b8cc913f98d7f39990
Guptaashish2003 <ashish.10119011622@ipu.ac.in>
2023-11-17 17:57:59 +0530

    working on admin panel using redux not completed

--- src/app/ContextProvider.js
+++ src/app/ContextProvider.js
@@ -1,37 +1,36 @@
-import React, { createContext, useContext, useState } from 'react';
+// app/ContextProvider.js
+import React, { createContext, useContext } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setColor, setMode, setThemeSettings } from '../redux/actions';
 
 const StateContext = createContext();
 
-const initialState = {
-  chat: false,
-  cart: false,
-  userProfile: false,
-  notification: false,
-};
-
 export const ContextProvider = ({ children }) => {
-  const [screenSize, setScreenSize] = useState(undefined);
-  const [currentColor, setCurrentColor] = useState('#03C9D7');
-  const [currentMode, setCurrentMode] = useState('Light');
-  const [themeSettings, setThemeSettings] = useState(false);
-  const [activeMenu, setActiveMenu] = useState(true);
-  const [isClicked, setIsClicked] = useState(initialState);
+  const dispatch = useDispatch();
+  const { currentColor, currentMode, themeSettings } = useSelector((state) => state);
 
-  const setMode = (e) => {
-    setCurrentMode(e.target.value);
+  const handleClick = (clicked) => {
+    // Use Redux actions here
+    if (clicked === 'themeSettings') {
+      dispatch(setThemeSettings(true));
+    }
+    // Add similar dispatch calls for other clicked values if needed
+  };
+
+  const setModeHandler = (e) => {
+    // Use Redux actions here
+    dispatch(setMode(e.target.value));
     localStorage.setItem('themeMode', e.target.value);
   };
 
-  const setColor = (color) => {
-    setCurrentColor(color);
+  const setColorHandler = (color) => {
+    // Use Redux actions here
+    dispatch(setColor(color));
     localStorage.setItem('colorMode', color);
   };
 
-  const handleClick = (clicked) => setIsClicked({ ...initialState, [clicked]: true });
-
   return (
-    // eslint-disable-next-line react/jsx-no-constructed-context-values
-    <StateContext.Provider value={{ currentColor, currentMode, activeMenu, screenSize, setScreenSize, handleClick, isClicked, initialState, setIsClicked, setActiveMenu, setCurrentColor, setCurrentMode, setMode, setColor, themeSettings, setThemeSettings }}>
+    <StateContext.Provider value={{ currentColor, currentMode, themeSettings, handleClick, setModeHandler, setColorHandler }}>
       {children}
     </StateContext.Provider>
   );
