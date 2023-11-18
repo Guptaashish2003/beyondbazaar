e0b15b705a5f9cd952aa804c8e7ec65dbd6aeaf1
Guptaashish2003 <ashish.10119011622@ipu.ac.in>
2023-11-18 13:01:43 +0530

    h

--- src/app/(pages)/show-address/page.js
+++ src/app/(pages)/show-address/page.js
@@ -1,17 +1,15 @@
-import React from 'react'
+import React from "react";
 import { FaPlus } from "react-icons/fa";
 
 const page = () => {
   return (
-
-    < div className='mt-40 mx-8 w-[80%]'>
-    <p className='text-lg text-gray-900 font-semibold' >Your Addresses</p>
-    <div className='flex w-40 h-40  items-center rounded-sm cursor-pointer justify-center text-center border-dashed border-2 ring-offset-2 ring-2 ring-gray-500'>
-        <FaPlus/>
-
+    <div className="mt-40 mx-8 w-[80%]">
+      <p className="text-lg text-gray-900 font-semibold">Your Addresses</p>
+      <div className="flex w-40 h-40  items-center rounded-sm cursor-pointer justify-center text-center border-dashed border-2 ring-offset-2 ring-2 ring-gray-500">
+        <FaPlus />
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
