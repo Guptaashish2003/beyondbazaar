d941ee364f1b9da71ec4fb752e3833e5e8ea84d1
Guptaashish2003 <ashish.10119011622@ipu.ac.in>
2023-11-19 11:49:48 +0530

    correcttion kar de account setting me css sahi kar

--- src/components/ProfileSetting/YourAccSetting.jsx
+++ src/components/ProfileSetting/YourAccSetting.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const YourAccSetting = ({img,title,description}) => {
   return (
-    <div className='w-[31%] max-sm:w-[45%] border-solid border-3  active:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 border-gray-400 cursor-pointer rounded-sm ring-offset-2 ring-2 ring-gray-500  hover:text-black'>
+    <div className='w-full max-sm:w-[45%] border-solid border-3  active:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 border-gray-400 cursor-pointer rounded-sm ring-offset-2 ring-2 ring-gray-500  hover:text-black'>
         <section className='w-full flex flex-col justify-center  bg-gray-50'>
             <div className="flex max-sm:flex-col max-md:items-center">
                 <div className="w-[20%] p-2 max-sm:w-1/2">
