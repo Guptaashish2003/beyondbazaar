2aaf1401abd9eec5934f5f77a67333bfc016a435
Guptaashish2003 <ashish.10119011622@ipu.ac.in>
2023-11-21 23:14:35 +0530

    account setting and page setup like professionals

---
+++ src/app/(pages)/user/account-setting/page.js
@@ -0,0 +1,88 @@
+"use client";
+import React from "react";
+import YourAccSetting from "@/components/ProfileSetting/YourAccSetting";
+import { FaShoppingBasket } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
+import { MdOutlineSecurity } from "react-icons/md";
+import { FaRegCreditCard } from "react-icons/fa";
+import { MdSupportAgent } from "react-icons/md";
+import { PiSignOutBold } from "react-icons/pi";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { signOut } from "next-auth/react";
+
+const Page = () => {
+  const router = useRouter();
+
+  const handleYourOrders = () => {
+    router.push("/your-orders");
+  };
+
+  const handleYourAddress = () => {
+    router.push("/user/your-address");
+  };
+
+  const handleSignOut = async () => {
+    const data = await signOut({ redirect: false });
+    if (data) {
+      router.push("/");
+    }
+  };
+
+  return (
+    <div className="mt-40  w-3/4 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-10 max-sm:gap-4 mx-auto my-4">
+      <Link href="your-orders"  passHref>
+        <YourAccSetting
+          onClick={handleYourOrders}
+          img={<FaShoppingBasket className="w-1/3  h-20 mx-auto max-md:w-1/2 object-cover" />}
+          title={"Your Orders"}
+          description={"Track Orders, Buy again "}
+        />
+      </Link>
+
+      <Link href="/user/your-address" passHref>
+        <YourAccSetting
+          onClick={handleYourAddress}
+          img={<FaLocationDot className="w-1/3 h-20 mx-auto max-md:w-1/2 object-cover" />}
+          title={"Your Addresses"}
+          description={"Edit your address for your gifts and orders  "}
+        />
+      </Link>
+
+      <Link href="/user/your-payment-options" passHref>
+        <YourAccSetting
+          img={<MdOutlineSecurity className="w-1/3 h-20 mx-auto max-md:w-1/2 object-cover" />}
+          title={"Login & Security"}
+          description={"Edit login, name, mobile number, email "}
+        />
+      </Link>
+
+      <Link href="/user/your-address" passHref>
+        <YourAccSetting
+          img={<FaRegCreditCard className="w-1/3 h-20 mx-auto max-md:w-1/2 object-cover" />}
+          title={"Payment Options"}
+          description={"Edit or add your payment options "}
+        />
+      </Link>
+
+      <Link href="/user/your-address" passHref>
+        <YourAccSetting
+          img={<MdSupportAgent className="w-1/3 h-20 mx-auto max-md:w-1/2 object-cover" />}
+          title={"Help & Support"}
+          description={"Contact us, and we'll be happy to assist you, just like family."}
+        />
+      </Link>
+
+      <Link href="/" passHref>
+        <YourAccSetting
+          onClick={handleSignOut}
+          img={<PiSignOutBold className="w-1/3 h-20 mx-auto max-md:w-1/2 object-cover" />}
+          title={"Log Out"}
+          description={"Thank you for visiting. I can't wait to meet you again."}
+        />
+      </Link>
+    </div>
+  );
+};
+
+export default Page;
