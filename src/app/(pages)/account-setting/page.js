c08a0951d68a51f363588fcb3be1ac43ba7a8685
Guptaashish2003 <ashish.10119011622@ipu.ac.in>
2023-11-21 20:09:35 +0530

    page link

--- src/app/(pages)/account-setting/page.js
+++ src/app/(pages)/account-setting/page.js
@@ -30,10 +30,9 @@ const Page = () => {
   };
 
   return (
-<<<<<<< HEAD
     <div className="mt-40 flex w-11/12 md:max-lg:justify-center max-sm:justify-center flex-row gap-10 max-sm:gap-4 mx-auto my-4 flex-wrap justify-start">
       <YourAccSetting
-      href="/your-orders"
+        href="/your-orders"
         onClick={handleYourOrders}
         img={<FaShoppingBasket className="w-1/2 h-1/2 object-cover" />}
         title={"Your Orders"}
@@ -68,63 +67,63 @@ const Page = () => {
         title={"Log Out"}
         description={"Thank  you for visiting. I can't wait to meet you again."}
       />
-=======
-    <div className="mt-40  w-11/12 md:max-lg:justify-center max-sm:justify-center flex-row gap-10 max-sm:gap-4 mx-auto my-4  justify-start">
-      <Link href="/your-orders" className="w-full inline-block" passHref>
-        <YourAccSetting
-          onClick={handleYourOrders}
-          img={<FaShoppingBasket className="w-1/2 h-1/2 object-cover" />}
-          title={"Your Orders"}
-          description={"Track Orders, Buy again "}
-        />
-      </Link>
+      =======
+      <div className="mt-40  w-11/12 md:max-lg:justify-center max-sm:justify-center flex-row gap-10 max-sm:gap-4 mx-auto my-4  justify-start">
+        <Link href="/your-orders" className="w-full inline-block" passHref>
+          <YourAccSetting
+            onClick={handleYourOrders}
+            img={<FaShoppingBasket className="w-1/2 h-1/2 object-cover" />}
+            title={"Your Orders"}
+            description={"Track Orders, Buy again "}
+          />
+        </Link>
 
-      <Link href="/your-address" className="w-full inline-block" passHref>
-        <YourAccSetting
-          onClick={handleYourAddress}
-          img={<FaLocationDot className="w-1/2 h-1/2 object-cover" />}
-          title={"Your Addresses"}
-          description={"Edit your address for your gifts and orders  "}
-        />
-      </Link>
+        <Link href="/your-address" className="w-full inline-block" passHref>
+          <YourAccSetting
+            onClick={handleYourAddress}
+            img={<FaLocationDot className="w-1/2 h-1/2 object-cover" />}
+            title={"Your Addresses"}
+            description={"Edit your address for your gifts and orders  "}
+          />
+        </Link>
 
-      <Link href="/your-address" className="w-full inline-block" passHref>
-        <YourAccSetting
-          img={<MdOutlineSecurity className="w-1/2 h-1/2 object-cover" />}
-          title={"Login & Security"}
-          description={"Edit login, name, mobile number, email "}
-        />
-      </Link>
+        <Link href="/your-address" className="w-full inline-block" passHref>
+          <YourAccSetting
+            img={<MdOutlineSecurity className="w-1/2 h-1/2 object-cover" />}
+            title={"Login & Security"}
+            description={"Edit login, name, mobile number, email "}
+          />
+        </Link>
 
-      <Link href="/your-address" className="w-full inline-block" passHref>
-        <YourAccSetting
-          img={<FaRegCreditCard className="w-1/2 h-1/2 object-cover" />}
-          title={"Payment Options"}
-          description={"Edit or add your payment options "}
-        />
-      </Link>
+        <Link href="/your-address" className="w-full inline-block" passHref>
+          <YourAccSetting
+            img={<FaRegCreditCard className="w-1/2 h-1/2 object-cover" />}
+            title={"Payment Options"}
+            description={"Edit or add your payment options "}
+          />
+        </Link>
 
-      <Link href="/your-address" className="w-full inline-block" passHref>
-        <YourAccSetting
-          img={<MdSupportAgent className="w-1/2 h-1/2 object-cover" />}
-          title={"Help & Support"}
-          description={
-            "Contact us, and we'll be happy to assist you, just like family."
-          }
-        />
-      </Link>
+        <Link href="/your-address" className="w-full inline-block" passHref>
+          <YourAccSetting
+            img={<MdSupportAgent className="w-1/2 h-1/2 object-cover" />}
+            title={"Help & Support"}
+            description={
+              "Contact us, and we'll be happy to assist you, just like family."
+            }
+          />
+        </Link>
 
-      <Link href="/" className="w-full inline-block" passHref>
-        <YourAccSetting
-          onClick={handleSignOut}
-          img={<PiSignOutBold className="w-1/2 h-1/2 object-cover" />}
-          title={"Log Out"}
-          description={
-            "Thank you for visiting. I can't wait to meet you again."
-          }
-        />
-      </Link>
->>>>>>> d941ee364f1b9da71ec4fb752e3833e5e8ea84d1
+        <Link href="/" className="w-full inline-block" passHref>
+          <YourAccSetting
+            onClick={handleSignOut}
+            img={<PiSignOutBold className="w-1/2 h-1/2 object-cover" />}
+            title={"Log Out"}
+            description={
+              "Thank you for visiting. I can't wait to meet you again."
+            }
+          />
+        </Link>
+      </div>
     </div>
   );
 };
