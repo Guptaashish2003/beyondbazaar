4bdb22b5104c3e703e622d871427145dbb04cf5f
Guptaashish2003 <ashish.10119011622@ipu.ac.in>
2023-11-26 20:42:49 +0530

    login security done

--- src/app/(pages)/user/login-security/page.js
+++ src/app/(pages)/user/login-security/page.js
@@ -1,6 +1,5 @@
 "use client";
 import React from "react";
-import Content from "@/components/Form/Content";
 import Modal from "@/components/Modal/Modal";
 import { toast } from "react-toastify";
 import InputBtn from "@/components/Form/InputBtn";
@@ -13,9 +12,12 @@ import { useUpdateDataProtected } from "@/redux/api/useUpdateData";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { useSelector } from "react-redux";
+
 const page = () => {
   const router = useRouter();
-  const [verification, setVerification] = useState(false);
+  const [verificationName, setVerificationName] = useState(false);
+  const [verificationNumber, setVerificationNumber] = useState(false);
+  const [verificationPassword, setVerificationPassword] = useState(false);
   const [loading, setLoading] = useState();
   const [passwordVisible, setPasswordVisible] = useState(false);
   const isVerify = useSelector((state) => state.loginSecurity.verification);
@@ -27,7 +29,6 @@ const page = () => {
   });
   if (!isVerify) {
     router.back();
-    
   }
 
   const passwordValidationSchema = Yup.object().shape({
@@ -40,18 +41,29 @@ const page = () => {
       .min(6, "Password must be at least 6 characters long")
       .oneOf([Yup.ref("new_password"), null], "Passwords must match"),
   });
+  const numberValidationSchema = Yup.object().shape({
+    number: Yup.string()
+      .required("Number is required")
+      .min(10, "Number must be at least 10 characters long")
+      .max(10, "Number must be at least 10 characters long")
+      .matches(/^[0-9]+$/, "Must be only digits"),
+  });
 
   const formOptionsName = { resolver: yupResolver(nameValidationSchema) };
-  const formOptionsPassword = {
-    resolver: yupResolver(passwordValidationSchema),
-  };
+  const formOptionsPassword = { resolver: yupResolver(passwordValidationSchema) };
+  const formOptionsNumber = { resolver: yupResolver(numberValidationSchema) };
 
   const {
     register: registerName,
     handleSubmit: handleSubmitName,
-    
+
     formState: { errors: errorsName },
   } = useForm(formOptionsName);
+  const {
+    register: registerNumber,
+    handleSubmit: handleSubmitNumber,
+    formState: { errors: errorsNumber },
+  } = useForm(formOptionsNumber);
 
   const {
     register: registerPassword,
@@ -59,15 +71,13 @@ const page = () => {
     formState: { errors: errorsPassword },
   } = useForm(formOptionsPassword);
   const changeName = async (data) => {
-    
-
     try {
       setLoading(true);
       const user = await useUpdateDataProtected("/api/user/update/", data);
       if (user.success) {
-        setVerification(true);
-      
+        setVerificationName(true);
       }
+      
       setLoading(false);
     } catch (error) {
       console.error("Error in change Name", error);
@@ -75,14 +85,33 @@ const page = () => {
       setLoading(false);
     }
   };
-  const changePassword = async (data) => {
-
+  const changeNumber = async (data) => {
     try {
       setLoading(true);
-      const user = await useUpdateDataProtected("/api/user/update-password", data);
+      const user = await useUpdateDataProtected("/api/user/update/", data);
       if (user.success) {
-        setVerification(true);
+        setVerificationNumber(true);
       }
+      setLoading(false);
+      
+    } catch (error) {
+      console.error("Error in change Number", error);
+      toast.error(error.message);
+      setLoading(false);
+    }
+  };
+  
+  const changePassword = async (data) => {
+    try {
+      setLoading(true);
+      const user = await useUpdateDataProtected(
+        "/api/user/update-password",
+        data
+      );
+      if (user.success) {
+        setVerificationPassword(true);
+      }
+      
       setLoading(false);
     } catch (error) {
       console.error("Error in changePassword:", error);
@@ -92,9 +121,9 @@ const page = () => {
   };
 
   return (
-    <div className="border-2 border-solid border-gray-500 mt-32 w-1/2 mx-auto">
-      <div className=" mx-auto w-full flex justify-between items-center p-5">
-        <div className="flex items-center text-center flex-col gap-3">
+    <div className=" flex  flex-col gap-2 mt-36 w-1/2 mx-auto">
+      <div className=" mx-auto border-2 border-solid border-gray-500 w-full flex justify-between items-center p-5">
+        <div className="flex items-start flex-col gap-3">
           <h1 className="text-black text-lg">Name</h1>
           <p className="text-gray-800">{"data.name"}</p>
         </div>
@@ -103,11 +132,12 @@ const page = () => {
           btnClass={`w-16 border border-gray-600 rounded-md`}
           btnName={"edit"}
         >
-          {verification ? (
+          {verificationName ? (
             <div>
               <h2 className="font-bold text-center text-2xl text-green-500">
                 Name change Succesfully
               </h2>
+              
             </div>
           ) : (
             <form
@@ -126,7 +156,7 @@ const page = () => {
               />
 
               {errorsName?.name && (
-                <p className="text-red-500 text-xs">{errors?.name?.message}</p>
+                <p className="text-red-500 text-xs">{errorsName.name.message}</p>
               )}
               <SubmitButton
                 loading={loading}
@@ -140,9 +170,71 @@ const page = () => {
         </Modal>
       </div>
       <hr className="mt-4 border-1 border-slate-500 mx-auto " />
-      
-      <div className=" mx-auto w-full flex justify-between items-center p-5">
-        <div className="flex items-center text-center flex-col gap-3">
+      <div className=" mx-auto border-2 border-solid border-gray-500 w-full flex justify-between items-center p-5">
+        <div className="flex items-start flex-col gap-3">
+          <h1 className="text-black text-lg">Contact Number</h1>
+          <p className="text-gray-800">{"data.name"}</p>
+        </div>
+
+        <Modal
+          btnClass={`w-16 border border-gray-600 rounded-md`}
+          btnName={"edit"}
+        >
+          {verificationNumber ? (
+            <div>
+              <h2 className="font-bold text-center text-2xl text-green-500">
+                Phone Number Updated Succesfully
+              </h2>
+              
+            </div>
+          ) : (
+            <form
+              onSubmit={handleSubmitNumber(changeNumber)}
+              className="flex flex-col gap-4 py-2"
+            >
+              <h2 className="font-bold text-center text-2xl text-[--first-color]">
+                Contact Number
+              </h2>
+              <InputBtn
+                type="tel"
+                name="Number"
+                placeholder="Enter your Phone Number"
+                {...registerNumber(
+                  "number",
+                  { required: "Numbe r is required" },
+                  {
+                    minLength: 10,
+                    message: "Number must be at least 10 characters long",
+                  },
+                  {
+                    maxLength: 10,
+                    message: "Number must be at least 10 characters long",
+                  },
+                  { pattern: /^[0-9]+$/, message: "Must be only digits" }
+                )}
+                className="px-8 py-2 rounded-md font-medium  border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+              />
+              {errorsNumber?.number && (
+                <p className="text-red-500 text-xs">
+                  {errorsNumber?.number?.message}
+                </p>
+              )}
+
+              <SubmitButton
+                loading={loading}
+                value="Change Number"
+                type={"submit"}
+                // onClick={handleSubmit(changeName)}
+                className="bg-[--first-color] rounded-sm text-white py-2 hover:scale-105 duration-300"
+              />
+            </form>
+          )}
+        </Modal>
+      </div>
+      <hr className="mt-4 border-1 border-slate-500 mx-auto " />
+
+      <div className=" mx-auto border-2 border-solid border-gray-500 w-full flex justify-between items-center p-5">
+        <div className="flex items-start flex-col gap-3">
           <h1 className="text-black text-lg">{"title"}</h1>
           <p className="text-gray-800">{"description"}</p>
         </div>
@@ -150,7 +242,7 @@ const page = () => {
           btnClass={`w-16 border border-gray-600 rounded-md`}
           btnName={"edit"}
         >
-          {verification ? (
+          {verificationPassword ? (
             <div>
               <h2 className="font-bold text-center text-2xl text-green-500">
                 Password update Succesfully
@@ -214,7 +306,6 @@ const page = () => {
                     <AiFillEye className="text-[--eye-color]" />
                   )}
                 </button>
-              
               </div>
               <div className="relative">
                 <InputBtn
@@ -247,18 +338,6 @@ const page = () => {
           )}
         </Modal>
       </div>
-      <hr className="mt-4 border-1 border-slate-500 mx-auto " />
-      <div className=" mx-auto w-full flex justify-between items-center p-5">
-        <div className="flex items-center text-center flex-col gap-3">
-          <h1 className="text-black text-lg">{"title"}</h1>
-          <p className="text-gray-800">{"description"}</p>
-        </div>
-        </div>
-      {/* <Content title={"name"} btnName={"edit"} description={"Ashish Gupta"} />
-      <Content title={"name"} btnName={"edit"} description={"Ashish Gupta"} />
-      <Content title={"name"} btnName={"edit"} description={"Ashish Gupta"} />
-      <Content title={"name"} btnName={"edit"} description={"Ashish Gupta"} />
-      <Content title={"name"} btnName={"edit"} description={"Ashish Gupta"} /> */}
     </div>
   );
 };
