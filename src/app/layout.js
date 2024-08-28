7392e134d94e7da0958733cb51a72910cec8db0b
Ashish Gupta <gkashish1985@gmail.com>
2024-08-29 00:01:16 +0530

    testing

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,6 +21,7 @@ export const metadata = {
 
 export default function RootLayout({children,params}) {
   const {isAdmin} = params
+  console.log(process.env.FRONTENDURL,"............");
   
   return (
     <html lang="en">
