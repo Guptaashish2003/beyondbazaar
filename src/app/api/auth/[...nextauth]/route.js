29a474602cdabbc25c1c225b88874d2c7c4355f4
Ashish Gupta <gkashish1985@gmail.com>
2024-09-01 17:22:56 +0530

    fixing

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,59 +1,64 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import User from "@/backend/model/User"
-import ConnectDB from "@/backend/DATABASE/ConnectDB"
-import { cookies } from 'next/headers'
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import User from "@/backend/model/User";
+import ConnectDB from "@/backend/DATABASE/ConnectDB";
+import { cookies } from "next/headers";
 
 const cookieOptions = {
   expires: new Date(
     Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
   ),
   secure: false,
-  
 };
 const handler = NextAuth({
-    session: {
-      strategy: "jwt",
-    },
-  providers:[
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
     GoogleProvider({
-        clientId:process.env.ClIENT_ID,
-        clientSecret:process.env.CLIENT_SECRET,
-    })
+      clientId: process.env.ClIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+    }),
   ],
-  
-    pages: {
-      signIn: "/login",
-      signOut: "/",
-      error: "/login",
-      
+  trustHost: true,
+
+  pages: {
+    signIn: "/login",
+    signOut: "/",
+    
+  },
+  callbacks: {
+    async session({ session, token, user }) {
+      await ConnectDB();
+
+      const { name, email } = session.user;
+      const exist = await User.findOne({ email });
+      var newUser;
+      var userToken;
+      if (!exist) {
+        newUser = await User.create({
+          name,
+          email,
+          byGoogle: true,
+          twoStepVerification: false,
+          byGooglePass: true,
+          isEmailValid: true,
+        });
+        userToken = await newUser.getSignedToken();
+      }
+      userToken = await exist.getSignedToken();
+      session.token = userToken;
+      cookies().set({
+        name: "token",
+        value: userToken,
+        httpOnly: true,
+        path: "/",
+        expires: cookieOptions.expires,
+      });
+      return session;
     },
-    callbacks: {
-      async session({ session, token, user }) {
-        await ConnectDB()
-  
-        const {name,email} = session.user;
-        const exist = await User.findOne({email});
-        var newUser;
-        var userToken;
-        if (!exist) {
-          newUser = await User.create({name, email,byGoogle: true,twoStepVerification:false,
-            byGooglePass:true,isEmailValid:true});
-          userToken = await newUser.getSignedToken();
-        }
-        userToken = await exist.getSignedToken();
-        session.token = userToken;
-        cookies().set({
-          name: "token",
-          value: userToken,
-          httpOnly: true,
-          path: '/',
-          expires: cookieOptions.expires,
-        })
-        return session;
-      },
   },
   secret: process.env.NEXTAUTH_SECRET,
-})
+});
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
