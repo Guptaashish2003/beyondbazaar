d1fb3f482482c1e15cb35c52a0dd93eb9ab86c3c
Ashish Gupta <gkashish1985@gmail.com>
2024-09-01 15:21:33 +0530

    refund policy

---
+++ src/app/(pages)/(beyond-bazaar-console)/RefundPolicy/page.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="w-1/3 mx-auto pt-24">
+      <p>
+        <p className="font-semibold text-xl">Cancellation & Refund Policy</p> 
+        Last updated on 01-09-2024 15:16:36 RAMESH
+        KUMAR GUPTA believes in helping its customers as far as possible, and
+        has therefore a liberal cancellation policy. Under this policy: •
+        Cancellations will be considered only if the request is made immediately
+        after placing the order. However, the cancellation request may not be
+        entertained if the orders have been communicated to the
+        vendors/merchants and they have initiated the process of shipping them.
+        • RAMESH KUMAR GUPTA does not accept cancellation requests for
+        perishable items like flowers, eatables etc. However, refund/replacement
+        can be made if the customer establishes that the quality of product
+        delivered is not good. • In case of receipt of damaged or defective
+        items please report the same to our Customer Service team. The request
+        will, however, be entertained once the merchant has checked and
+        determined the same at his own end. This should be reported within 2
+        Days days of receipt of the products. In case you feel that the product
+        received is not as shown on the site or as per your expectations, you
+        must bring it to the notice of our customer service within 2 Days days
+        of receiving the product. The Customer Service Team after looking into
+        your complaint will take an appropriate decision. • In case of
+        complaints regarding products that come with a warranty from
+        manufacturers, please refer the issue to them. In case of any Refunds
+        approved by the RAMESH KUMAR GUPTA, it’ll take 6-8 Days days for the
+        refund to be processed to the end customer.Cancellation & Refund Policy
+        Last updated on 01-09-2024 15:16:36 RAMESH KUMAR GUPTA believes in
+        helping its customers as far as possible, and has therefore a liberal
+        cancellation policy. Under this policy: • Cancellations will be
+        considered only if the request is made immediately after placing the
+        order. However, the cancellation request may not be entertained if the
+        orders have been communicated to the vendors/merchants and they have
+        initiated the process of shipping them. • RAMESH KUMAR GUPTA does not
+        accept cancellation requests for perishable items like flowers, eatables
+        etc. However, refund/replacement can be made if the customer establishes
+        that the quality of product delivered is not good. • In case of receipt
+        of damaged or defective items please report the same to our Customer
+        Service team. The request will, however, be entertained once the
+        merchant has checked and determined the same at his own end. This should
+        be reported within 2 Days days of receipt of the products. In case you
+        feel that the product received is not as shown on the site or as per
+        your expectations, you must bring it to the notice of our customer
+        service within 2 Days days of receiving the product. The Customer
+        Service Team after looking into your complaint will take an appropriate
+        decision. • In case of complaints regarding products that come with a
+        warranty from manufacturers, please refer the issue to them. In case of
+        any Refunds approved by the RAMESH KUMAR GUPTA, it’ll take 6-8 Days days
+        for the refund to be processed to the end customer.
+      </p>
+    </div>
+  );
+};
+
+export default page;
